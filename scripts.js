b57c731beea486312ac4fd0da02e1cfbac296835
alextryonpdx <alextryonpdx@gmail.com>
2018-09-17 19:51:56 -0700

    building js validation (simple scoring) function

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
 
     console.log( "ready!" );
 
+    SimpleScoringModel('#hpi_validate input', '#hpi_score')
 });
 
 
@@ -16,3 +17,51 @@ $("#next-section-history a").click(function() {
         scrollTop: $("#history-section").offset().top
     }, 750);
 });
+
+
+
+
+function SimpleScoringModel(group, echo){
+	//on keyup for any input in a defined group...
+	$(group).on('keyup', function(){
+		//set a variable to track score
+		score = 0
+		//iterate through inputs in the group
+		$(group).each(function(){
+			//if the input has any value (not blank)
+			if($(this).val().length){
+				//increment the variable
+				score++;
+			}
+		})
+		//after calculating total, echo the result to the document
+		$(echo).html('hpi score: ' + score );
+	})
+}
+
+
+
+
+function HPI_Model(){
+	//on keyup for any input in a defined group...
+	$('#hpi_validate input').on('keyup', function(){
+		//set a variable to track score
+		hpi_score = 0
+		//iterate through inputs in the group
+		$('#hpi_validate input').each(function(){
+			//if the input has any value (not blank)
+			if($(this).val().length){
+				//increment the variable
+				hpi_score++;
+			}
+		})
+		//after calculating total, echo the result to the document
+		$('h1#hpi_score').html('hpi score: ' + hpi_score );
+	})
+}
+
+
+
+
+
+
