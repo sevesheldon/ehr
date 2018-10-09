7e28828d46a744a0889aaaf91a1482398171359f
alextryonpdx <alextryonpdx@gmail.com>
2018-10-08 20:04:37 -0700

    m

--- validation.js
+++ validation.js
@@ -24,6 +24,46 @@ validationScores = {
 		physical: 2,
 		mdm : 1
 	},
+	99203 : {
+		history : 3,
+		physical: 3,
+		mdm : 2
+	},
+	99204 : {
+		history : 4,
+		physical: 4,
+		mdm : 3
+	},
+	99205 : {
+		history : 4,
+		physical: 4,
+		mdm : 4
+	},
+	99211 : {
+		history : 0,
+		physical: 0,
+		mdm : 0
+	},
+	99212 : {
+		history : 1,
+		physical: 1,
+		mdm : 1
+	},
+	99213 : {
+		history : 2,
+		physical: 2,
+		mdm : 2
+	},
+	99214 : {
+		history : 3,
+		physical: 3,
+		mdm : 3
+	},
+	99215 : {
+		history : 5,
+		physical: 5,
+		mdm : 5
+	},
 }
 
 
