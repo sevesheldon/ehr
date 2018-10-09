ffb7b1c3c61e366d379bab08617557760c94807d
alextryonpdx <alextryonpdx@gmail.com>
2018-10-08 19:48:32 -0700

    simplify validation object

--- validation.js
+++ validation.js
@@ -3,39 +3,27 @@
 //store current validation points
 currentScore = {
 	history : score,
-	hpi : score,
-	ros : score,
-	pfsh : score,
-	exam : score,
+	physical: score
 	mdm : score
 }
 //validation boolean array
 validation = {
 	history : false,
-	hpi : false,
-	ros : false,
-	pfsh : false,
-	exam : false,
+	physical : false,
 	mdm : false
 }
 //set of validation rules
 validationScores = {
 	targetCode : {
 		history : score,
-		hpi : score,
-		ros : score,
-		pfsh : score,
-		exam : score,
+		physical: score,
 		mdm : score
 	},
 	targetCode : {
 		history : score,
-		hpi : score,
-		ros : score,
-		pfsh : score,
-		exam : score,
+		physical: score,
 		mdm : score
-	}
+	},
 }
 
 
@@ -43,18 +31,22 @@ validationScores = {
 if(currentScore[history] >= targetCode[history]){
 	validation[history] = true;
 } else { validation[history] = false }
-if(currentScore[hpi] >= targetCode[hpi]){
-	validation[hpi] = true;
-} else { validation[hpi] = false }
-if(currentScore[ros] >= targetCode[ros]){
-	validation[ros] = true;
-} else { validation[ros] = false }
-if(currentScore[pfsh] >= targetCode[pfsh]){
-	validation[pfsh] = true;
-} else { validation[pfsh] = false }
-if(currentScore[exam] >= targetCode[exam]){
-	validation[exam] = true;
-} else { validation[exam] = false }
+
+if(currentScore[physical] >= targetCode[physical]){
+	validation[physical] = true;
+} else { validation[physical] = false }
+
 if(currentScore[mdm] >= targetCode[mdm]){
 	validation[mdm] = true;
 } else { validation[mdm] = false }
+
+
+
+
+
+
+
+
+
+
+
